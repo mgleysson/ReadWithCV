8c69196e3733acb6a73926b081984a579ed38ae4
danilofernandes45 <danilofernandes45@gmail.com>
2018-08-24 12:46:27 -0300

    Visualization with Coefficient of Variation

---
+++ read_with_cv.R
@@ -0,0 +1,23 @@
+library(raster)
+library(png)
+
+hhhh <- raster("hhhh.grd")
+hvhv <- raster("hvhv.grd")
+vvvv <- raster("vvvv.grd")
+
+my_cv <- function(x, y) return (sd(x)/mean(x))
+
+hhhh_cv <- localFun(hhhh, hhhh, ngb = 3, fun = my_cv, filename = "hhhh_cv.grd")
+hvhv_cv <- localFun(hvhv, hvhv, ngb = 3, fun = my_cv, filename = "hvhv_cv.grd")
+vvvv_cv <- localFun(vvvv, vvvv, ngb = 3, fun = my_cv, filename = "vvvv_cv.grd")
+
+nrow <- nrow(hhhh_cv)
+ncol <- ncol(hhhh_cv)
+
+matrix_cv <- array(0, dim = c(nrow, ncol, 3))
+
+matrix_cv[,,1] <- getValuesBlock(hhhh_cv, nrow = 1, nrows = nrow, ncol = 1, ncols = ncol, format = 'matrix')
+matrix_cv[,,2] <- getValuesBlock(hvhv_cv, nrow = 1, nrows = nrow, ncol = 1, ncols = ncol, format = 'matrix')
+matrix_cv[,,3] <- getValuesBlock(vvvv_cv, nrow = 1, nrows = nrow, ncol = 1, ncols = ncol, format = 'matrix')
+
+writePNG(matrix_cv, target = "image.png")
